725750254618f139d2ec00054f83413de713aac6
Maicon Pinto <maiconsilva.pinto@gmail.com>
2023-05-31 16:31:50 -0300

    SecundarioComponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecundarioComponent } from './secundario/secundario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecundarioComponent
   ],
   imports: [
     BrowserModule,
